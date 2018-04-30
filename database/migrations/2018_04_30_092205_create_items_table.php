6e9ead8c9745aca177a73fcb8b6eb3f4f1a7ec02
Matiullah Karimi <karimi@netlinks.ws>
2018-04-30 15:25:18 +0430

    Add Category, Item and Purchase migrations, controllers and models

---
+++ database/migrations/2018_04_30_092205_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->string('name');
+            $table->text('desc')->nullable();
+            $table->integer('price');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
---
+++ database/migrations/2018_04_30_092459_create_purchases_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('item_id')->unsigned();
+            $table->string('username');
+            $table->integer('phone')->nullable();
+            $table->integer('paid');
+            $table->integer('sold_price');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->foreign('item_id')->references('id')->on('items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+}
